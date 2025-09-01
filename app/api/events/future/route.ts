1a04549af33b452d03a0ea38be8aaa417cb9b177
jbirsa <jbirsa@itba.edu.ar>
2025-09-01 13:12:27 -0300

    Added some api routes

---
+++ app/api/events/future/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse, NextRequest } from "next/server";
+import { Evento } from "@/types/db_types";
+import { getSupabaseServerClient } from "@/lib/supabaseClient";
+
+
+export async function GET(req: NextRequest) {
+    try {   
+        const supabase = getSupabaseServerClient();
+        const { data, error } = await supabase.from('evento').select('*').gte('fecha', new Date().toISOString().split('T')[0]);
+        if(error)
+            return NextResponse.json( {error: error}, { status: 500 } );
+        if(!data)
+            return NextResponse.json( { error: 'No data found' }, { status: 404 } );
+
+        const res = data.map((event: Evento) => {
+            return {
+                id: event.id,
+                name: event.nombre,
+                type: event.tipo,
+                date: event.fecha,
+                location: event.direccion,
+                description: event.descripcion,
+                link: event.link,
+                barrio: event.barrio,
+                provincia: event.provincia,
+            }
+        });
+        return NextResponse.json({ events: res }, { status: 200, headers: { 'Content-Type': 'application/json' } });
+    } catch (error){
+        return NextResponse.json( {error: error}, { status: 500 } );
+    }
+}
